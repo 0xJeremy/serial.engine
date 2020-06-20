462ca6300f95b78b9cb8d2d55e17d10ab062de0e
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-06-20 19:55:43 -0400

    Linted with Black

--- python/serialengine/connection.py
+++ python/serialengine/connection.py
@@ -21,81 +21,79 @@ from .constants import BAUD_RATE
 ### CONNECTION CLASS ###
 ########################
 
+
 class connection:
-	def __init__(self, port, baud=BAUD_RATE, timeout=TIMEOUT, size=SIZE):
-		self.canWrite = True
-		self.channels = {}
-		self.size = size
-		self.timeout = timeout
-		self.stopped = False
-		self.opened = False
-		self.port = port
-		self.baud = baud
-		self.serial = serial.Serial(port, baudrate=baud, timeout=self.timeout)
-		self.lock = Lock()
+    def __init__(self, port, baud=BAUD_RATE, timeout=TIMEOUT, size=SIZE):
+        self.canWrite = True
+        self.channels = {}
+        self.size = size
+        self.timeout = timeout
+        self.stopped = False
+        self.opened = False
+        self.port = port
+        self.baud = baud
+        self.serial = serial.Serial(port, baudrate=baud, timeout=self.timeout)
+        self.lock = Lock()
 
-	def start(self):
-		Thread(target=self.__run, args=()).start()
-		return self
+    def start(self):
+        Thread(target=self.__run, args=()).start()
+        return self
 
-	def __run(self):
-		tmp = ''
-		while True:
-			if self.stopped:
-				self.serial.close()
-				return
+    def __run(self):
+        tmp = ""
+        while True:
+            if self.stopped:
+                self.serial.close()
+                return
 
-			tmp += self.serial.readline(self.size).decode()
-			
-			if tmp == 'READY':
-				self.opened = True
-				tmp = ''
+            tmp += self.serial.readline(self.size).decode()
 
-			if tmp != '':
-				data = tmp.split('\n')
-				for i in range(len(data)):
-					try:
-						msg = jsonToDict(data[i])
-					except JSONDecodeError:
-						continue
+            if tmp == "READY":
+                self.opened = True
+                tmp = ""
 
-					self.__cascade(msg[TYPE], msg[DATA])
-					self.channels[msg[TYPE]] = msg[DATA]
-					data[i] = ''
+            if tmp != "":
+                data = tmp.split("\n")
+                for i in range(len(data)):
+                    try:
+                        msg = jsonToDict(data[i])
+                    except JSONDecodeError:
+                        continue
 
-				tmp = ''.join(data)
+                    self.__cascade(msg[TYPE], msg[DATA])
+                    self.channels[msg[TYPE]] = msg[DATA]
+                    data[i] = ""
 
-	def __cascade(self, mtype, mdata):
-		if mtype == ACK:
-			self.canWrite = True
-		if mtype == STATUS:
-			if mdata == CLOSING:
-				self.__close()
-		if mtype == NAME_CONN:
-			self.name = mdata
-		if mtype == IMAGE:
-			self.write(ACK, ACK)
-		return
+                tmp = "".join(data)
 
-	def __close(self):
-		self.opened = False
-		self.stopped = True
+    def __cascade(self, mtype, mdata):
+        if mtype == ACK:
+            self.canWrite = True
+        if mtype == STATUS:
+            if mdata == CLOSING:
+                self.__close()
+        if mtype == NAME_CONN:
+            self.name = mdata
+        if mtype == IMAGE:
+            self.write(ACK, ACK)
+        return
 
-	def get(self, channel):
-		with self.lock:
-			if channel in self.channels.keys():
-				return self.channels[channel]
-			return None
+    def __close(self):
+        self.opened = False
+        self.stopped = True
 
-	def write(self, channel, data):
-		if self.opened:
-			with self.lock:
-				msg = {
-					TYPE: channel.replace('\n', ''),
-					DATA: data.replace('\n', '')
-				}
-				self.serial.write(dictToJson(msg).encode() + NEWLINE)
+    def get(self, channel):
+        with self.lock:
+            if channel in self.channels.keys():
+                return self.channels[channel]
+            return None
 
-	def close(self):
-		self.write(STATUS, CLOSING)
-		self.__close()
+    def write(self, channel, data):
+        if self.opened:
+            with self.lock:
+                msg = {TYPE: channel.replace("\n", ""), DATA: data.replace("\n", "")}
+                self.serial.write(dictToJson(msg).encode() + NEWLINE)
+
+    def close(self):
+        self.write(STATUS, CLOSING)
+        self.__close()
--- python/test.py
+++ python/test.py
@@ -1,16 +1,16 @@
 from serialengine.connection import connection
 import time
 
-conn = connection('/dev/ttyACM0', baud=2000000, timeout=1).start()
+conn = connection("/dev/ttyACM0", baud=2000000, timeout=1).start()
 
 while not conn.opened:
-	pass
+    pass
 
-conn.write('test', 'hello there')
+conn.write("test", "hello there")
 
-while conn.get('test') == None:
-	pass
+while conn.get("test") == None:
+    pass
 
-print("SUCCESS:", conn.get('test'))
+print("SUCCESS:", conn.get("test"))
 
 conn.close()
